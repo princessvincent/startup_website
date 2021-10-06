842cfbf4b600a3547e7839b6e68469a4e76ed06b
princessvincent <pricavincent2018@gmail.com>
2021-10-05 17:26:42 -0700

    edited

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+let first_name = "prisca"
+
+console.log("hello " + first_name);
+
+const footballers = {
+    bace:8 , man:9, prisca: "girl"}
+
+console.table(footballers)
+
+const array = [
+    ['Work', 9],
+    ['Eat', 1],
+    ['Commute', 2],
+    ['Play Game', 1],
+    ['Sleep', 7]
+]
+console .table(array)
